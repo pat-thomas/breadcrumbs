4ab6c6de1be180df54907c4914bd88de4cc2df90
Pat Thomas <patthomassoftware@gmail.com>
2014-05-09 22:39:03 -0400

    trace-fn now captures exceptions and logs exception type, exception message, and arguments that caused exception.

--- src/breadcrumbs/core.clj
+++ src/breadcrumbs/core.clj
@@ -8,15 +8,22 @@
      (let [namespace (keyword (str *ns*))]
        (-> debug-atom
            deref
-           (get-in [namespace (keyword (name fn-name))])
+           (get-in [namespace (keyword (name fn-name)) :calls])
            last)))
   ([fn-name arg-name]
      (let [namespace (keyword (str *ns*))]
        (-> debug-atom
            deref
-           (get-in [namespace (keyword (name fn-name))])
+           (get-in [namespace (keyword (name fn-name)) :calls])
            last
            (get (keyword (name arg-name)))))))
+
+(defmacro lookup-exceptions
+  ([fn-name]
+     (let [namespace (keyword (str *ns*))]
+       (-> debug-atom
+           deref
+           (get-in [namespace (keyword (name fn-name)) :exceptions])))))
 
 (defmacro trace-fn
   "When wrapped around a defn, redefines function to trace its calls"
@@ -24,15 +31,24 @@
   (let [[_ fn-name fn-args fn-body] body
         debug-key [(keyword (str *ns*)) (keyword fn-name)]]
     (when (empty? (get-in @debug-atom debug-key))
-      (swap! debug-atom assoc-in debug-key []))
+      (let [nascent-debug-map {:calls      []
+                               :exceptions []}]
+       (swap! debug-atom assoc-in debug-key nascent-debug-map)))
     `(defn ~fn-name ~fn-args
        (let [capture-map# (reduce (fn [acc# [arg-sym# arg-val#]]
                                     (let [arg-key# (keyword (name arg-sym#))]
                                       (assoc acc# arg-key# arg-val#)))
                                   {}
                                   (partition 2 (interleave '~fn-args ~fn-args)))]
-         (swap! debug-atom update-in ~debug-key #(conj % capture-map#)))
-       ~fn-body)))
+         (swap! debug-atom update-in ~(conj debug-key :calls) #(conj % capture-map#))
+         (try
+           ~fn-body
+           (catch Exception e#
+             (let [exception-rec# {:message (.getMessage e#)
+                                   :type    (.getClass e#)
+                                   :args    capture-map#}]
+               (swap! debug-atom update-in ~(conj debug-key :exceptions) #(conj % exception-rec#))
+               (throw e#))))))))
 
 (defmacro untrace-fn
   "Given a function name, allows to be called with args captured by trace-fn"
